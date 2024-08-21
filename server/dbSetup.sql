1937331af8c60cce6d1166683d65b9d5eb476982
Zach Barnes <zachary.barnes.32@gmail.com>
2024-08-21 16:08:09 -0600

    Created Ingredients Table

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -28,3 +28,14 @@ FROM
   JOIN accounts ON accounts.id = recipes.creatorId
   WHERE
     recipes.id = 1
+
+CREATE TABLE 
+ingredients(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  name VARCHAR(255) NOT NULL,
+  quantity VARCHAR(255) NOT NULL,
+  recipeId INT NOT NULL,
+  FOREIGN KEY (recipeId) REFERENCES recipes (id) ON DELETE CASCADE
+)
